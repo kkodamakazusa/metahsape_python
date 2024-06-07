25974499f519189960bfcc780883edacb53ec8db
kodama Kunihiro <74333186+kkodamakazusa@users.noreply.github.com>
2024-06-07 15:28:33 +0900

    Update test.bat

--- test.bat
+++ test.bat
@@ -2,8 +2,8 @@ rem @echo off
 set METASHAPE_PATH="C:\Program Files\Agisoft\Metashape Pro\metashape.exe"
 set SCRIPT_DIR=%~dp0
 
-rem %METASHAPE_PATH% -r %SCRIPT_DIR%\make_align.py
-rem %METASHAPE_PATH% -r %SCRIPT_DIR%\set_drone_coord.py
+%METASHAPE_PATH% -r %SCRIPT_DIR%\make_align.py
+%METASHAPE_PATH% -r %SCRIPT_DIR%\set_drone_coord.py
 rem %METASHAPE_PATH% -r %SCRIPT_DIR%\c_test2.py
-%METASHAPE_PATH% -r %SCRIPT_DIR%\bdens.py
+rem %METASHAPE_PATH% -r %SCRIPT_DIR%\bdens.py
 pause
